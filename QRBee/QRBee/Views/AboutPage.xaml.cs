e63435665cddfa169f4facc0b4e51d568e6c5c84
Andrey Shabarshov <andrew.shabarshov@gmail.com>
2021-11-26 17:04:27 +0000

    Add project files.

---
+++ QRBee/QRBee/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QRBee.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QRBee/QRBee/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QRBee.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QRBee.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
