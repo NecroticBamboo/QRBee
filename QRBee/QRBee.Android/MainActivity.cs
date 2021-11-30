11f751ed2399ba889888c0e3f824ca8776aa8c27
Andrey Shabarshov <andrew.shabarshov@gmail.com>
2021-11-30 18:07:43 +0000

    QR code scanner works.

--- QRBee/QRBee.Android/MainActivity.cs
+++ QRBee/QRBee.Android/MainActivity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+using Android;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Support.V4.Content;
+using AndroidX.Core.App;
 
 namespace QRBee.Droid
 {
@@ -17,11 +19,21 @@ namespace QRBee.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            ZXing.Mobile.MobileBarcodeScanner.Initialize(Application);
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.Camera) == (int) Permission.Granted)
+            {
+                
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.Camera }, 0);
+            }
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
